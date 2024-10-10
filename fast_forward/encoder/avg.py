a7df2b05ba23f4ededb676c1a7815e06d37654ff
Bo van den Berg <bvdb98@gmail.com>
2024-10-10 17:12:12 +0200

    Reformat AvgEncoder

--- fast_forward/encoder/avg.py
+++ fast_forward/encoder/avg.py
@@ -1,6 +1,7 @@
 from typing import Sequence
 
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 from fast_forward.encoder import Encoder
 from fast_forward.index import Index
@@ -19,25 +20,25 @@ class AvgEncoder(Encoder):
         k_avg: int,
     ) -> None:
         super().__init__()
-        self.sparse_ranking = sparse_ranking
+        self.top_ranking = sparse_ranking.cut(k_avg)
         self.index = index
-        self.k_avg = k_avg
 
 
     def __call__(self, queries: Sequence[str]) -> np.ndarray:
-        q_reps: np.ndarray = np.zeros((len(self.sparse_ranking), self.index.dim), dtype=np.float32)
-        top_docs = self.sparse_ranking.cut(self.k_avg)
+        q_reps: np.ndarray = np.zeros((len(self.top_ranking), self.index.dim), dtype=np.float32)
+        
         for query in tqdm(queries, desc="Estimating query embeddings", total=len(queries)):
-            # Find [q_id, q_no] from sparse_ranking filtered on the query
-            # TODO: Make line below more readable
-            q_id, q_no = self.sparse_ranking._df[self.sparse_ranking._df['query'] == query][['q_id', 'q_no']].iloc[0]
-            
-            # get the embeddings of the top_docs from the index
-            top_docs_ids = top_docs[q_id].keys()
-            d_reps: np.ndarray = self.index._get_vectors(top_docs_ids)[0]
+            # Get the ids of the top-ranked documents for the query
+            top_docs: pd.DataFrame = self.top_ranking._df.query("query == @query")
+            top_docs_ids: Sequence[int] = top_docs["id"].values
+
+            # Get the embeddings of the top-ranked documents
+            d_reps: np.ndarray = self.index._get_vectors(top_docs_ids)[0]            
             if self.index.quantizer is not None:
                 d_reps = self.index.quantizer.decode(d_reps)
 
-            # calculate the average of the embeddings and save it
+            # Calculate the average of the embeddings and save it to q_no index in q_reps
+            q_no = top_docs["q_no"].iloc[0]
             q_reps[q_no] = np.mean(d_reps, axis=0)
+        
         return q_reps
