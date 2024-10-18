ecade3d19a2df5bcecc96e25ed67417a9bedebef
Bo van den Berg <bvdb98@gmail.com>
2024-10-18 17:11:18 +0200

    Add --max_queries program arg to sparse_retrieval.py

--- usage/sparse_retrieval.py
+++ usage/sparse_retrieval.py
@@ -26,6 +26,7 @@ def parse_args():
     parser.add_argument("--topics_variant", type=str, default="test-2019", help="Topics variant.")
     parser.add_argument("--k", type=int, default=10000, help="Number of documents to re-rank per query.")
     parser.add_argument("--out_dir", type=str, default="/home/bvdb9/sparse_rankings", help="Output directory.")
+    parser.add_argument("--max_queries", type=int, default=None, help="Maximum number of queries to process.")
     return parser.parse_args()
 
 
@@ -41,6 +42,8 @@ def main(
     if not pt.started():
         pt.init()
 
+    output_file_suffix = ""
+
     # Retrieve BM25 model from the batchretrieve index
     print(f"Retrieving {args.retriever_wmodel} model from {args.dataset} dataset...")
     bm25 = pt.BatchRetrieve.from_dataset(args.dataset, args.retriever_variant, wmodel=args.retriever_wmodel, verbose=True)
@@ -50,14 +53,17 @@ def main(
         topics = pt.get_dataset(args.dataset).get_topics()
     else:
         topics = pt.get_dataset(args.dataset).get_topics(args.topics_variant)
+    if args.max_queries is not None and args.max_queries < len(topics):
+        print(f"Limiting the number of queries to {args.max_queries} (out of {len(topics)}).")
+        topics = topics.head(args.max_queries)
+        output_file_suffix = f"-{args.max_queries}queries"
     print(f"topics:\n{topics}")
 
     top_ranked_docs = (bm25 % args.k)(topics)
     print(f"top_ranked_docs (Length: {len(topics)} topics * {args.k} docs = {len(top_ranked_docs)}):\n{top_ranked_docs}")
 
     # Write to the sparse_runfile.tsv
-    output_file = os.path.join(args.out_dir, f"{args.dataset}-{args.topics_variant}-{args.retriever_wmodel}-top{args.k}.tsv")
-    print("\nWriting to", output_file)
+    output_file = os.path.join(args.out_dir, f"{args.dataset}-{args.topics_variant}-{args.retriever_wmodel}-top{args.k}{output_file_suffix}.tsv")
     with open(output_file, "w") as f:
         # top_ranked_docs.to_csv("sparse_runfile.csv", sep=" ", header=False, index=False)
         for i, row in tqdm(top_ranked_docs.iterrows(), total=len(top_ranked_docs), desc=f"Writing results to {output_file}"):
