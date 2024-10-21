d796cb804888a3b8721a4122b0e26a52579321ad
Bo van den Berg <bvdb98@gmail.com>
2024-10-21 15:05:16 +0200

    Add ir_dataset topics to sparse_retrieval.py

--- usage/sparse_retrieval.py
+++ usage/sparse_retrieval.py
@@ -23,8 +23,9 @@ def parse_args():
     parser.add_argument("--dataset", type=str, default="msmarco_passage", help="Dataset for testing (using package ir-datasets).")
     parser.add_argument("--retriever_variant", type=str, default="terrier_stemmed", help="Retriever variant.")
     parser.add_argument("--retriever_wmodel", type=str, default="BM25", help="Retriever weighting model.")
-    parser.add_argument("--topics_variant", type=str, default="test-2019", help="Topics variant.")
-    parser.add_argument("--k", type=int, default=10000, help="Number of documents to re-rank per query.")
+    parser.add_argument("--pt_topics_variant", type=str, default=None, help="Topics variant. Example: 'test-2019'")
+    parser.add_argument("--ir_dataset_topics", type=str, default=None, help="Topics variant. Example: 'msmarco-passage/trec-dl-2019/judged'")
+    parser.add_argument("--k", type=int, default=100, help="Number of documents to re-rank per query.")
     parser.add_argument("--out_dir", type=str, default="/home/bvdb9/sparse_rankings", help="Output directory.")
     parser.add_argument("--max_queries", type=int, default=None, help="Maximum number of queries to process.")
     return parser.parse_args()
@@ -49,10 +50,15 @@ def main(
     retriever = pt.BatchRetrieve.from_dataset(args.dataset, args.retriever_variant, wmodel=args.retriever_wmodel, verbose=True)
 
     # Get the test topics (dataframe with columns=['qid', 'query'])
-    if args.topics_variant is None:
-        topics = pt.get_dataset(args.dataset).get_topics()
+    if args.pt_topics_variant is not None:
+        topics = pt.get_dataset(args.dataset).get_topics(args.pt_topics_variant)
+        topics_str = args.pt_topics_variant
+    elif args.ir_dataset_topics is not None:
+        topics = pt.get_dataset(args.ir_dataset_topics).get_topics()
+        topics_str = args.ir_dataset_topics.split("/", 1)[-1].replace("/", ".")
     else:
-        topics = pt.get_dataset(args.dataset).get_topics(args.topics_variant)
+        raise ValueError("Either --pt_topics_variant or --ir_dataset_topics must be provided.")
+
     if args.max_queries is not None and args.max_queries < len(topics):
         print(f"Limiting the number of queries to {args.max_queries} (out of {len(topics)}).")
         topics = topics.head(args.max_queries)
@@ -63,7 +69,7 @@ def main(
     print(f"top_ranked_docs (Length: {len(topics)} topics * {args.k} docs = {len(top_ranked_docs)}):\n{top_ranked_docs}")
 
     # Write to the sparse_runfile.tsv
-    output_file = os.path.join(args.out_dir, f"{args.dataset}-{args.topics_variant}-{args.retriever_wmodel}-top{args.k}{output_file_suffix}.tsv")
+    output_file = os.path.join(args.out_dir, f"{args.dataset}-{topics_str}-{args.retriever_wmodel}-top{args.k}{output_file_suffix}.tsv")
     with open(output_file, "w") as f:
         # top_ranked_docs.to_csv("sparse_runfile.csv", sep=" ", header=False, index=False)
         for i, row in tqdm(top_ranked_docs.iterrows(), total=len(top_ranked_docs), desc=f"Writing results to {output_file}"):
