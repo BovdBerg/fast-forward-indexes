64a948b27612cb3d59e628b5597d713bf1d354a9
Bo van den Berg <bvdb98@gmail.com>
2024-12-11 19:06:52 +0100

    Fix train model

--- usage/train.py
+++ usage/train.py
@@ -42,41 +42,21 @@ print('Validation set has {} instances'.format(len(validation_set)))
 
 
 ### Model
-class AvgWeightsClassifier(nn.Module):
+class LearnedAvgWeights(nn.Module):
     def __init__(self):
         super().__init__()
-        # TODO: rewrite method to fit my needs
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.weights = nn.Parameter(torch.ones(10))
+        print("Model initialized as {}".format(self))
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 4 * 4)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-    # def forward(self, d_reps: np.ndarray) -> Sequence[float]:
-    #     """Forward pass of the model.
-
-    #     Args:
-    #         d_reps (np.ndarray): List of top-ranked document embeddings for a given query.
-
-    #     Returns:
-    #         weights (Sequence[float]): Learned weights for each document embedding based on position/scores.
-    #     """
-    #     # TODO: rewrite method to fit my needs
-    #     uniform = np.ones(len(d_reps)) / len(d_reps)
-    #     return uniform
+    def forward(self, d_reps: np.ndarray) -> Sequence[float]:
+        # Softmax: Normalize weights to sum to 1
+        weights = F.softmax(self.weights, dim=0)
+        # Compute the weighted average of the input embeddings
+        q_rep = torch.sum(weights.unsqueeze(1) * d_reps, dim=0)
+        return q_rep
 
 
-model = AvgWeightsClassifier()
+model = LearnedAvgWeights()
 # model.load_state_dict(torch.load(model_path))
 
 
