1e9e0b3836b6334322052c49c18af28a525450a5
Bo van den Berg <bvdb98@gmail.com>
2024-12-13 16:14:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+# Read the requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='fast_forward',
-    version='0.3',
-    packages=['fast_forward'],  # Explicitly specify the 'fast_forward' package
-    # Other arguments like author, description, etc.
+    version='0.5.2',
+    packages=find_packages(),
+    install_requires=required,
+    author="Jurek Leonhardt (https://github.com/mrjleo), Bo van den Berg (https://github.com/bovdberg)",
 )
