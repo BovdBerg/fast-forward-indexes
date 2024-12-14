5d873f9d20a2fb6c333cf73e0efe192c508537a3
Bo van den Berg <bvdb98@gmail.com>
2024-12-14 22:50:21 +0100

    Minor update to use_trained_weights.py

--- usage/use_trained_weights.py
+++ usage/use_trained_weights.py
@@ -8,6 +8,7 @@ from fast_forward.encoder.avg import LearnedAvgWeights
 ckpt_path = "/home/bvdb9/fast-forward-indexes/lightning_logs/version_8/checkpoints/epoch=0-step=995.ckpt"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = LearnedAvgWeights.load_from_checkpoint(ckpt_path).to(device)
+model.eval()  # disable randomness, dropout, etc.
 
 # embed some fake top docs (= d_reps)! model(X) calls model.forward(X). Output is weighted average of d_reps.
 X = torch.rand(10 * 768, device=model.device)
