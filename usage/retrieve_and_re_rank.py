fbd888a317119aa59c8b3bf23a2debd131fca449
Bo van den Berg <bvdb98@gmail.com>
2024-12-16 22:17:28 +0100

    Change metric for validation

--- usage/retrieve_and_re_rank.py
+++ usage/retrieve_and_re_rank.py
@@ -148,7 +148,7 @@ def parse_args():
     parser.add_argument(
         "--dev_eval_metric",
         type=str,
-        default="ndcg_cut_10",  # Find official metrics for dataset version on https://ir-datasets.com/msmarco-passage.html
+        default="map",  # Find official metrics for dataset version on https://ir-datasets.com/msmarco-passage.html
         help="Evaluation metric for pt.GridSearch on dev set.",
     )
     parser.add_argument(
