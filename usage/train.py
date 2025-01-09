811c8d55c1b2172417197e7a0129b7f52f1698f6
Bo van den Berg <bvdb98@gmail.com>
2025-01-09 01:54:17 +0100

    Load emb_ckpt (bert) before training

--- usage/train.py
+++ usage/train.py
@@ -53,6 +53,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--ckpt_emb_path",
         type=Path,
+        default="/home/bvdb9/models/emb_768.ckpt",
         help="Path to the BERT checkpoint file to load.",
     )
     parser.add_argument(
