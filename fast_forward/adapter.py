1c878e6e67eadf52c477a010774f664d71b3b70c
Bo van den Berg <bvdb98@gmail.com>
2025-01-11 00:15:06 +0100

    Create simple adapter model

---
+++ fast_forward/adapter.py
@@ -0,0 +1,23 @@
+import torch
+from fast_forward.util.lightning import GeneralModule
+
+
+class Adapter(GeneralModule):
+    def __init__(self):
+        super().__init__()
+        self.flatten = torch.nn.Flatten(0)
+
+        hidden_dim = 1000
+        self.linear_relu_stack = torch.nn.Sequential(
+            torch.nn.Linear(768, hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim, 768)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.flatten(x)
+        x = self.linear_relu_stack(x)
+        return x
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
