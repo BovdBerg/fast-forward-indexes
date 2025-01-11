7542769016643bc0a3ed9d58d9e77068268fcf6f
Bo van den Berg <bvdb98@gmail.com>
2025-01-11 03:36:33 +0100

    Remove flatten from adapter model

--- fast_forward/adapter.py
+++ fast_forward/adapter.py
@@ -7,7 +7,6 @@ from fast_forward.lightning import GeneralModule
 class Adapter(GeneralModule):
     def __init__(self, ckpt_path: Optional[str] = None, device: str = "cpu") -> None:
         super().__init__()
-        self.flatten = torch.nn.Flatten(0)
 
         encoding_dim = 768
         hidden_dim = 100
@@ -28,7 +27,5 @@ class Adapter(GeneralModule):
         self.linear_relu_stack.eval()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.flatten(x)
         x = self.linear_relu_stack(x)
-        x = x.unsqueeze(0).unsqueeze(0)
         return x
