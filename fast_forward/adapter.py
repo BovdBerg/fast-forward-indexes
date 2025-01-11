6448ac9568c35b284951af3be645fdeffa68c794
Bo van den Berg <bvdb98@gmail.com>
2025-01-11 01:58:30 +0100

    Improve Adapter Model

--- fast_forward/adapter.py
+++ fast_forward/adapter.py
@@ -1,4 +1,5 @@
 import torch
+
 from fast_forward.util.lightning import GeneralModule
 
 
@@ -7,14 +8,16 @@ class Adapter(GeneralModule):
         super().__init__()
         self.flatten = torch.nn.Flatten(0)
 
-        hidden_dim = 1000
+        encoding_dim = 768
+        hidden_dim = 100
         self.linear_relu_stack = torch.nn.Sequential(
-            torch.nn.Linear(768, hidden_dim),
+            torch.nn.Linear(encoding_dim, hidden_dim),
             torch.nn.ReLU(),
-            torch.nn.Linear(hidden_dim, 768)
+            torch.nn.Linear(hidden_dim, encoding_dim),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.flatten(x)
         x = self.linear_relu_stack(x)
+        x = x.unsqueeze(0).unsqueeze(0)
         return x
