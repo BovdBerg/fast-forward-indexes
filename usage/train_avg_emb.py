29550ac4f1a790bcf6dc8f1f55496d20f22c03b6
Bo van den Berg <bvdb98@gmail.com>
2025-01-22 02:05:45 +0100

    Less strict earlystopping

--- usage/train_avg_emb.py
+++ usage/train_avg_emb.py
@@ -204,7 +204,7 @@ def main() -> None:
         callbacks=[
             callbacks.ModelCheckpoint(monitor="val_loss", verbose=True),
             callbacks.EarlyStopping(
-                monitor="val_loss", min_delta=1e-3, patience=5, verbose=True
+                monitor="val_loss", min_delta=1e-4, patience=5, verbose=True
             ),
         ],
     )
