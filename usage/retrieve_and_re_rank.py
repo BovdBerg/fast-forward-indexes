4c78cc48acb7fd588dab52688d5e0984cb148393
Bo van den Berg <bvdb98@gmail.com>
2025-01-27 07:43:45 +0100

    Only append to gsheets if passed

--- usage/retrieve_and_re_rank.py
+++ usage/retrieve_and_re_rank.py
@@ -368,17 +368,18 @@ def main(args: argparse.Namespace) -> None:
             settings_str = print_settings()
             print(f"\nFinal results on {test_dataset_name}:\n{results}\n")
 
-            # Save new results to Google Sheets
-            if not PREV_RESULTS.exists() or str(results) != str(
-                pd.read_json(PREV_RESULTS)
-            ):
-                results.to_json(PREV_RESULTS, indent=4)
-                settings_str += f"\nTest: '{test_dataset_name}'"
-                append_to_gsheets(results, settings_str)
-            else:
-                print(
-                    "Results have not changed since the last run. Skipping Google Sheets update."
-                )
+            # Save new results to Google Sheets if gsheets_credentials exists
+            if args.gsheets_credentials:
+                if not PREV_RESULTS.exists() or str(results) != str(
+                    pd.read_json(PREV_RESULTS)
+                ):
+                    results.to_json(PREV_RESULTS, indent=4)
+                    settings_str += f"\nTest: '{test_dataset_name}'"
+                    append_to_gsheets(results, settings_str)
+                else:
+                    print(
+                        "Results have not changed since the last run. Skipping Google Sheets update."
+                    )
 
     end_time = time.time()
     print(f"Total time: {end_time - start_time:.2f} seconds.")
