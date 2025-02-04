55a4565ada179b1a2cf9923279abee7f78181fe6
Bo van den Berg <bvdb98@gmail.com>
2025-02-04 01:25:50 +0100

    f plots

--- plot_data/weight_methods.py
+++ plot_data/weight_methods.py
@@ -20,40 +20,43 @@ def parse_args():
 
 
 def plot(data: Dict[str, Any]):
+    n_docs = data["n_docs"]
+    weights = data["weights"]
+    pos_scores = data["pos_scores"]
+
     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    ax.set_xlim(0, n_docs)
 
-    ax.set_xlabel('Embeddings position (%)')
+    ax.set_xlabel('Document rank')
     ax.set_ylabel('Weight distribution (%)')
-    
-    # For each n_docs, plot the weights of the embeddings as a line plot with dots. The first element should be left and the last should be right.
-    for i in range(len(data)):
-        n_docs = data[i]["n_docs"]
-        weights = data[i]["weights"]
 
-        # Normalize weights
-        weights_normalized = np.array(weights)
-        weights_normalized = weights_normalized / np.sum(weights_normalized) * 100
+    # Normalize weights
+    weights_normalized = np.array(weights)
+    weights_normalized = weights_normalized / np.sum(weights_normalized) * 100
 
-        # Normalize x-axis values
-        x_values = np.linspace(0, n_docs, len(weights_normalized))
+    # Ignore the first element and normalize
+    weights = np.array(weights[1:])
+    weights = weights / np.sum(weights) * 100
+    x_values = np.linspace(0, n_docs, len(weights))
 
-        # Ignore the first element and normalize
-        weights = np.array(weights[1:])
-        weights = weights / np.sum(weights) * 100
-        x_values_ignored = np.linspace(0, 10, len(weights))
+    # Plot the weights as a line plot with dots
+    ax.plot(x_values, weights, marker='.', label=f"Learned")
 
-        # Plot the weights as a line plot with dots
-        ax.plot(x_values_ignored, weights, marker='.', label=f"Learned")
+    # Add a horizontal line for uniform weight distribution
+    uniform_weight = 100 / len(weights)
+    ax.axhline(y=uniform_weight, color='r', linestyle='--', label='Uniform')
 
-        # Add a horizontal line for uniform weight distribution
-        uniform_weight = 100 / len(weights)
-        ax.axhline(y=uniform_weight, color='r', linestyle='--', label='Uniform')
+    # Add exponential weight distribution
+    exp_factor = 0.4
+    exponential_approximation = np.array([np.exp(-i**exp_factor) for i in range(len(weights))])
+    exponential_approximation = exponential_approximation / np.sum(exponential_approximation) * 100
+    ax.plot(x_values, exponential_approximation, linestyle='--', label=f'Exponential decay (-x$^{{{exp_factor}}}$)')
 
-        # Add exponential weight distribution
-        exp_factor = 0.4
-        exponential_approximation = np.array([np.exp(-i**exp_factor) for i in range(len(weights))])
-        exponential_approximation = exponential_approximation / np.sum(exponential_approximation) * 100
-        ax.plot(x_values_ignored, exponential_approximation, linestyle='--', label=f'Exponential decay (-x$^{{{exp_factor}}}$)')
+    # Normalize and softmax position scores and plot them as weight distribution
+    pos_scores = np.array(pos_scores)
+    pos_scores_normalized = pos_scores / np.sum(pos_scores) * 100
+    softmax_pos_scores = np.exp(pos_scores_normalized) / np.sum(np.exp(pos_scores_normalized)) * 100
+    ax.plot(x_values, softmax_pos_scores, linestyle='--', label='Softmax norm. rank scores')
 
     ax.legend()
 
@@ -62,13 +65,11 @@ def plot(data: Dict[str, Any]):
 
 
 def main(args: argparse.Namespace) -> None:
-    data = [
-        {
-            "n_docs": 10,
-            "weights": [0.8738, 0.0441, 0.0265, 0.0189, 0.0122, 0.0087, 0.0063, 0.0040, 0.0027, 0.0018, 0.0010],
-            "pos_scores": [29.11000671, 27.71860574, 27.31457273, 26.505415, 26.12595619, 24.08121221, 23.78653777, 23.34701436, 23.03488101, 22.8207384]
-        },
-    ]
+    data = {
+        "n_docs": 10,
+        "weights": [0.8738, 0.0441, 0.0265, 0.0189, 0.0122, 0.0087, 0.0063, 0.0040, 0.0027, 0.0018, 0.0010],
+        "pos_scores": [29.11000671, 27.71860574, 27.31457273, 26.505415, 26.12595619, 24.08121221, 23.78653777, 23.34701436, 23.03488101, 22.8207384]
+    }
 
     plot(data)
 
