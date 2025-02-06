f5324712b656f106634901d4e185e441af3627d1
Bo van den Berg <bvdb98@gmail.com>
2025-02-06 17:53:48 +0100

    Improve naming

--- fast_forward/encoder/transformer_embedding.py
+++ fast_forward/encoder/transformer_embedding.py
@@ -46,15 +46,15 @@ class TransformerEmbeddingEncoder(torch.nn.Module):
 
     def forward(self, batch: BatchEncoding) -> torch.Tensor:
         # Get token embeddings
-        tokens_emb = self.embeddings(batch["input_ids"])
+        tok_embs = self.embeddings(batch["input_ids"])
 
         # Apply attention mask to remove padding tokens
         attention_mask = batch["attention_mask"]
-        masked_emb = tokens_emb * attention_mask.unsqueeze(-1)
+        masked_emb = tok_embs * attention_mask.unsqueeze(-1)
 
         # Compute the mean of the masked embeddings
-        lengths = attention_mask.sum(dim=1, keepdim=True)
-        mean_emb = masked_emb.sum(dim=1) / lengths
+        n_unmasked = attention_mask.sum(dim=1, keepdim=True)
+        mean_emb = masked_emb.sum(dim=1) / n_unmasked
 
         return mean_emb
 
