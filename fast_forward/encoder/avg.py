f6f17e0b5fe5c37147b9a8bb462c63ffed264aa7
Bo van den Berg <bvdb98@gmail.com>
2025-02-09 22:44:24 +0100

    Add minor weight offset to prevent div/0

--- fast_forward/encoder/avg.py
+++ fast_forward/encoder/avg.py
@@ -168,7 +168,7 @@ class AvgEmbQueryEstimator(Encoder, GeneralModule):
         mask: torch.Tensor,
     ) -> torch.Tensor:
         weights = init_weights * mask  # Mask padding
-        weights = weights / weights.sum(dim=-1, keepdim=True)  # Normalize
+        weights = weights / (weights.sum(dim=-1, keepdim=True) + 1e-8)  # Normalize
 
         embs = embs * weights.unsqueeze(-1)  # Apply weights
         q_estimation = embs.sum(-2)  # Compute weighted sum
