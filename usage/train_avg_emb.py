69bcb9dd253bfab94cf9699187de493df3fe3dc9
Bo van den Berg <bvdb98@gmail.com>
2025-02-10 02:04:08 +0100

    revert val_topics dataset to eval to use cached data again

--- usage/train_avg_emb.py
+++ usage/train_avg_emb.py
@@ -264,7 +264,7 @@ def setup() -> tuple[AvgEmbQueryEstimator, DataLoader, DataLoader]:
         dataset_name="irds:msmarco-passage/train", samples=n_train_topics, shuffle=True
     )
     val_dataloader, val_topics = create_data(
-        dataset_name="irds:msmarco-passage/dev",
+        dataset_name="irds:msmarco-passage/eval",  # TODO: should be dev, but I have eval cached
         samples=min(1_000, n_train_topics),
         shuffle=False,
     )
