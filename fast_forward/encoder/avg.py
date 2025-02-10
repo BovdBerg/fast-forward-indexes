7892370ba259562f888f00c232ae8f13b0a6d8ca
Bo van den Berg <bvdb98@gmail.com>
2025-02-10 02:34:01 +0100

    make retrieved vectors from index uniq again

--- fast_forward/encoder/avg.py
+++ fast_forward/encoder/avg.py
@@ -188,7 +188,7 @@ class AvgEmbQueryEstimator(Encoder, GeneralModule):
             LOGGER.info(f"1 (top_docs) ranking lookup took: {t1 - t0:.5f}s")
 
         # Retrieve any needed embeddings from the index
-        d_embs, d_idxs = self.index._get_vectors(top_docs["id"])
+        d_embs, d_idxs = self.index._get_vectors(top_docs["id"].unique())
         if self.index.quantizer is not None:
             d_embs = self.index.quantizer.decode(d_embs)
         d_embs = torch.tensor(
