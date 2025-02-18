a328622070db26b6f28ab94a2d3a9b5a8d51e788
Bo van den Berg <bvdb98@gmail.com>
2025-02-18 12:45:19 +0100

    Stricter Early stopping

--- usage/train_avg_emb.py
+++ usage/train_avg_emb.py
@@ -318,7 +318,7 @@ def main() -> None:
                 verbose=True,
             ),
             callbacks.EarlyStopping(
-                monitor="val_loss", min_delta=3e-4, patience=5, verbose=True
+                monitor="val_loss", min_delta=1e-4, patience=5, verbose=True
             ),
         ],
     )
