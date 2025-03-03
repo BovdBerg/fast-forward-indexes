72aefef0d7e628976a72fea5da77d17cb0956bb3
Bo van den Berg <bvdb98@gmail.com>
2025-03-03 01:21:14 +0100

    f

--- plot_data/performance_efficiency_graph.py
+++ plot_data/performance_efficiency_graph.py
@@ -33,7 +33,7 @@ def plot_runtimes(profiles: List[Dict[str, Any]]):
     ax2.set_ylim(bottom=0, top=latency_enc.max() * 1.1)
 
     # Plot for latency vs nDCG@10
-    ax1.set_xlabel('nDCG@10', fontsize=18)
+    ax1.set_xlabel("nDCG$_{10}$", fontsize=18)
     ax1.set_ylabel('Re-ranking latency (ms)', fontsize=18)
     for i in range(len(names)):
         ax1.scatter(nDCG[i], latency[i], label=names[i], marker='*' if i < 2 else 's', s=150, zorder=3 if i < 2 else 2)
@@ -41,15 +41,17 @@ def plot_runtimes(profiles: List[Dict[str, Any]]):
     ax1.set_title('Full re-ranking', fontsize=20)
 
     # Plot for latency_enc vs nDCG@10
-    ax2.set_xlabel('nDCG@10', fontsize=18)
+    ax2.set_xlabel("nDCG$_{10}$", fontsize=18)
     ax2.set_ylabel('Query-encoding latency (ms)', fontsize=18)
     for i in range(len(names)):
         ax2.scatter(nDCG[i], latency_enc[i], label=names[i], marker='*' if i < 2 else 's', s=150)
     ax2.legend(bbox_to_anchor=(0, 1), loc='upper left', fontsize=14)
     ax2.set_title('Query-encoding', fontsize=20)
 
-    fig.tight_layout()
+    ax1.grid(True, color='lightgray', linestyle='-', linewidth=0.5)
+    ax2.grid(True, color='lightgray', linestyle='-', linewidth=0.5)
 
+    fig.tight_layout()
     fig.savefig("plot_data/figures/performance_efficiency_graph.png", transparent=True)
     plt.show()
 
@@ -77,19 +79,19 @@ def main(args: argparse.Namespace) -> None:
             "nDCG@10": 0.677,
         },
         {
-            "name": "AvgEmb_docs",
+            "name": "AvgEmb$_{docs}$",
             "latency": 1636,
             "latency_enc": 64.8,
             "nDCG@10": 0.571,
         },
         {
-            "name": "AvgEmb_docs + AvgTokEmb",
+            "name": "AvgEmb$_{docs}$ + AvgTokEmb",
             "latency": 3350,
             "latency_enc": 68.59,
             "nDCG@10": 0.694,
         },
         {
-            "name": "AvgEmb",
+            "name": "AvgEmb$_{q,docs}$",
             "latency": 1671,
             "latency_enc": 75.98,
             "nDCG@10": 0.690,
--- plot_data/performance_efficiency_per_n_docs.py
+++ plot_data/performance_efficiency_per_n_docs.py
@@ -33,26 +33,29 @@ def plot_runtimes(profiles: List[Dict[str, Any]]):
     ax2.set_ylim(bottom=0, top=latency_enc.max() * 1.1)
 
     # Plot for latency vs nDCG@10
-    ax1.set_xlabel('nDCG@10', fontsize=18)
+    ax1.set_xlabel("nDCG$_{10}$", fontsize=18)
     ax1.set_ylabel('Re-ranking latency (ms)', fontsize=18)
     for i in range(len(names)):
         color = f"C{i}"
         if color >= "C3":
             color = f"C{i + 1}"
         ax1.scatter(nDCG[i], latency[i], label=names[i], marker='*' if i < 2 else 's', s=150, zorder=3 if i < 2 else 2, color=color)
-    ax1.legend(bbox_to_anchor=(0, 1), loc='upper left', fontsize=12)
+    ax1.legend(bbox_to_anchor=(0, 1), loc='upper left', fontsize=11)
     ax1.set_title('Full re-ranking', fontsize=20)
 
     # Plot for latency_enc vs nDCG@10
-    ax2.set_xlabel('nDCG@10', fontsize=18)
+    ax2.set_xlabel("nDCG$_{10}$", fontsize=18)
     ax2.set_ylabel('Query-encoding latency (ms)', fontsize=18)
     for i in range(len(names)):
         color = f"C{i}"
         if color >= "C3":
             color = f"C{i + 1}"
         ax2.scatter(nDCG[i], latency_enc[i], label=names[i], marker='*' if i < 2 else 's', s=150, zorder=3 if i < 2 else 2, color=color)
-    ax2.legend(bbox_to_anchor=(0, 1), loc='upper left', fontsize=12)
+    ax2.legend(bbox_to_anchor=(0, 1), loc='upper left', fontsize=11)
     ax2.set_title('Query-encoding', fontsize=20)
+
+    ax1.grid(True, color='lightgray', linestyle='-', linewidth=0.5)
+    ax2.grid(True, color='lightgray', linestyle='-', linewidth=0.5)
 
     fig.tight_layout()
     fig.savefig("plot_data/figures/performance_efficiency_per_n_docs.png", transparent=True)
@@ -82,28 +85,28 @@ def main(args: argparse.Namespace) -> None:
             "nDCG@10": 0.677,
         },
         {
-            "name": "n_docs=0 (=q_only)",
+            "name": "AvgEmb$_q$",
             "latency": 1636,
             "latency_enc": 15.49,
             "nDCG@10": 0.663,
         },
         {
-            "name": "n_docs=1",
+            "name": "AvgEmb$_{q,1-doc}$",
             "latency": 1677,
             "latency_enc": 55.27,
             "nDCG@10": 0.673,
         },
         {
-            "name": "n_docs=10",
+            "name": "AvgEmb$_{q,10-docs}$",
             "latency": 1687,
             "latency_enc": 74.77,
             "nDCG@10": 0.690,
         },
         {
-            "name": "n_docs=50",
+            "name": "AvgEmb$_{q,50-docs}$",
             "latency": 1691,
             "latency_enc": 79.53,
-            "nDCG@10": 0.689,
+            "nDCG@10": 0.689,  # 0.691
         },
     ]
 
